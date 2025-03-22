631bdecffbc01e283a591cf3ba8c121994c5b45e
d3crypt360 <nahilali3600@gmail.com>
2025-03-22 22:31:55 +0500

    webview added. Splash and icon added

--- App.tsx
+++ App.tsx
@@ -1,20 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React, { useRef, useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  StyleSheet,
+  BackHandler,
+} from "react-native";
+import { WebView } from "react-native-webview";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
   },
 });
+
+export default function App() {
+  const webViewRef = useRef<WebView>(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  // Function to handle back navigation
+  const handleBackPress = () => {
+    if (canGoBack && webViewRef.current) {
+      webViewRef.current.goBack();
+      return true; // Prevents app from closing
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (Platform.OS === "android") {
+      BackHandler.addEventListener("hardwareBackPress", handleBackPress);
+      return () =>
+        BackHandler.removeEventListener("hardwareBackPress", handleBackPress);
+    }
+  }, [canGoBack]);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        ref={webViewRef}
+        source={{ uri: "https://www.thaana.ai/" }}
+        allowsBackForwardNavigationGestures={true} // Enables swipe gestures on iOS
+        onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)} // Track back navigation state
+        userAgent={
+          Platform.OS === "android"
+            ? "Chrome/18.0.1025.133 Mobile Safari/535.19"
+            : "AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75"
+        }
+      />
+    </SafeAreaView>
+  );
+}
